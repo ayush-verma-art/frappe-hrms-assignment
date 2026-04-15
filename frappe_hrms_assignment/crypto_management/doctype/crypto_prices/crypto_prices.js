ee1dd14bcafa9fef657645c807c36bcf844f429d
ayush-verma-art <ayushverma041@gmail.com>
2026-04-16 03:13:50 +0530

    feat: added crypto management module with API-based price fetching functionality

---
+++ frappe_hrms_assignment/crypto_management/doctype/crypto_prices/crypto_prices.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ayush Verma and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Crypto Prices", {
+// 	refresh(frm) {
+
+// 	},
+// });
